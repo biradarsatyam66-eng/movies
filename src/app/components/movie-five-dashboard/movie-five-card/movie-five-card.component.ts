359d8d73b0bf6b77cc45e5ed342c6a51f2c8ae24
biradarsatyam66-eng <biradarsatyam66@gmail.com>
2026-09-19 00:51:11 +0530

    movie io

--- src/app/components/movie-five-dashboard/movie-five-card/movie-five-card.component.ts
+++ src/app/components/movie-five-dashboard/movie-five-card/movie-five-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Imovies } from 'src/app/model/movie.interface';
 
 @Component({
   selector: 'app-movie-five-card',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieFiveCardComponent implements OnInit {
 
+  @Input() getmovie !: Imovies
+  @Output() emitdeleteobj : EventEmitter<number> = new EventEmitter<number>()
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  ondelete(id:number){
+    this.emitdeleteobj.emit(id)
+  }
 }
--- src/app/components/movie-five-dashboard/movie-five-dashboard.component.ts
+++ src/app/components/movie-five-dashboard/movie-five-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { movieArray } from 'src/app/const/movie';
+import { Imovies } from 'src/app/model/movie.interface';
 
 @Component({
   selector: 'app-movie-five-dashboard',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieFiveDashboardComponent implements OnInit {
 
+
+  movieArr :Imovies[]= []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.movieArr = movieArray
+  }
+
+  getdeleteobj(id:number){
+    let getindex = this.movieArr.findIndex(i=>i.id === id)
+    this.movieArr.splice(getindex,1)
   }
 
 }
